f27d9c5f7988ef6d9c7a79b2c1d842331dce2885
Shiva Gaire <abiralsambhu@gmail.com>
2017-05-29 16:32:37 +0545

    Write code passing the failing test

--- q2.c
+++ q2.c
@@ -4,16 +4,61 @@
   License : MIT
 
   This code is compiled with GCC version 6.3.0 20170406
+
+  Note    : While compiling you need to pass `-lm` switch to let linker to link to additional files
+            required by the library. Because only the declaration is in `pow` of `math.h`, defintion
+            is elsewhere.
+  fixme:  This program has places for optimization; number of loops can be reduced. 
 */
 
+#include <math.h>
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct{
   int num1;
   int num2;
   int product;
 } Palindrom;
+
+int checkPalindrome(int num){
+  int dup = num;
+  int temp = 0;
+  while(num>0){
+   int  digit =  num%10;
+   temp = digit + temp*10;
+   num /= 10;
+  }
+  if(temp == dup)
+    return 1;
+  else
+    return 0;
+}
+
+Palindrom* find(int noOfDigits){
+  Palindrom* data = malloc(sizeof(Palindrom));
+  data->product   = 0;
+  int upperBound  = pow(10, noOfDigits) - 1;
+  int lowerBound  = pow(10, noOfDigits - 1);
+
+  for(int i = upperBound; i >= lowerBound; i--){
+    /* upperBound-- is to reduce the number of loops */
+    for(int j = upperBound--; j >= lowerBound; j--){
+      if(checkPalindrome(i*j) && data->product < i*j){
+        data->product = i*j;
+        data->num1 = j;
+        data->num2 = i;
+      }
+    }
+  }
+  return data;
+}
+
+void displayOutput(Palindrom* response){
+  printf("%d = %d x %d\n", response->product, response->num1, response->num2);
+}
 
 void main(){
   assert(checkPalindrome(9) == 1);
@@ -36,4 +81,9 @@ void main(){
   assert(response3->num1     == 913);
   assert(response3->num2     == 993);
 
+  displayOutput(response3);
 }
+
+/* SAMPLE OUTPUT
+  906609 = 913 x 993
+*/
